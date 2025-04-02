b162e9b4d893295c4c4727a53f22b987dcced1ac
cheesypops <djxviana@gmail.com>
2025-04-02 16:59:47 -0600

    Add: high impact hero

--- payload-testing/src/heros/HighImpact/index.tsx
+++ payload-testing/src/heros/HighImpact/index.tsx
@@ -8,7 +8,16 @@ import { CMSLink } from '@/components/Link'
 import { Media } from '@/components/Media'
 import RichText from '@/components/RichText'
 
-export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
+export const HighImpactHero: React.FC<Page['hero']> = (
+  { links, 
+    title, 
+    description,
+    media1,
+    media2,
+    media3,
+    primaryColor,
+    secondaryColor2
+  }) => {
   const { setHeaderTheme } = useHeaderTheme()
 
   useEffect(() => {
@@ -22,10 +31,20 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
     >
       <div className="container mb-8 z-10 relative flex items-center justify-center">
         <div className="max-w-[36.5rem] md:text-center">
-          {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
-          {Array.isArray(links) && links.length > 0 && (
-            <ul className="flex md:justify-center gap-4">
-              {links.map(({ link }, i) => {
+          {title && (
+            <h1 className="mb-4 text-4xl font-bold leading-tight md:text-5xl"
+            style={{color: primaryColor || 'white'}}
+            >
+              {title}
+            </h1>
+          )}
+          <div className="mb-4">
+            <p
+            style={{color: secondaryColor2 || 'white'}}
+            >{description}</p>
+          </div>
+          <ul className="flex md:justify-center gap-4">
+              {links && links.map(({ link }, i) => {
                 return (
                   <li key={i}>
                     <CMSLink {...link} />
@@ -33,12 +52,17 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
                 )
               })}
             </ul>
-          )}
         </div>
       </div>
       <div className="min-h-[80vh] select-none">
-        {media && typeof media === 'object' && (
-          <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
+        {media1 && typeof media1 === 'object' && (
+        <Media fill imgClassName="-z-10 object-cover" priority resource={media1} />
+        )}
+        {media2 && typeof media2 === 'object' && (
+          <Media fill imgClassName="-z-10 object-cover" priority resource={media2} />
+        )}
+        {media3 && typeof media3 === 'object' && (
+          <Media fill imgClassName="-z-10 object-cover" priority resource={media3} />
         )}
       </div>
     </div>
