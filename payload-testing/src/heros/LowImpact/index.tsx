60669ef3158b1ce92fe983797950a2daa381a23f
cheesypops <djxviana@gmail.com>
2025-04-02 17:08:17 -0600

    Add: medium impact tsx

--- payload-testing/src/heros/LowImpact/index.tsx
+++ payload-testing/src/heros/LowImpact/index.tsx
@@ -1,25 +1,44 @@
 import React from 'react'
 
 import type { Page } from '@/payload-types'
+import { CMSLink } from '@/components/Link'
 
-import RichText from '@/components/RichText'
 
-type LowImpactHeroType =
-  | {
-      children?: React.ReactNode
-      richText?: never
-    }
-  | (Omit<Page['hero'], 'richText'> & {
-      children?: never
-      richText?: Page['hero']['richText']
-    })
-
-export const LowImpactHero: React.FC<LowImpactHeroType> = ({ children, richText }) => {
+export const LowImpactHero: React.FC<Page['hero']> = ({ 
+  title,
+  description,
+  links,
+  primaryColor,
+  secondaryColor2,
+ }) => {
   return (
     <div className="container mt-16">
-      <div className="max-w-[48rem]">
-        {children || (richText && <RichText data={richText} enableGutter={false} />)}
-      </div>
+      <div className="container mb-8 z-10 relative flex items-center justify-center">
+              <div className="max-w-[36.5rem] md:text-center">
+                {title && (
+                  <h1 className="mb-4 text-4xl font-bold leading-tight md:text-5xl"
+                  style={{color: primaryColor || 'white'}}
+                  >
+                    {title}
+                  </h1>
+                )}
+                <div className="mb-4">
+                  <p
+                  style={{color: secondaryColor2 || 'white'}}
+                  >{description}</p>
+                </div>
+                <ul className="flex md:justify-center gap-4">
+                    {links && links.map(({ link }, i) => {
+                      return (
+                        <li key={i}>
+                          <CMSLink {...link} />
+                        </li>
+                      )
+                    })}
+                  </ul>
+              </div>
+            </div>
+            
     </div>
   )
 }
